195dfb70b6b4ce3bdd1fef8dc7ec4128e5e392ea
Paulinha Merlim <104771318+PaulaMerlim@users.noreply.github.com>
2022-07-20 13:04:11 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Faça login no CodeSandbox
+
+// Crie um novo sandbox em REACT
+
+// Crie um Component de function e retorne uma frase na tela
+
+import React from "react";
+export default function App() {
+  return (
+    <div>
+      <h1>Estou me esfornçando, quero chegar na final!</h1>
+    </div>
+  );
+}
